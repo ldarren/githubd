3eaf5ca7c6fbad68f86c8c616baf0ddd38f542c2
Darren Liew <ldarren@gmail.com>
2018-04-06 10:26:57 +0000

    added references

--- slack.js
+++ slack.js
@@ -1,3 +1,4 @@
+// https://tutorials.botsfloor.com/creating-a-slack-command-bot-from-scratch-with-node-js-distribute-it-25cf81f51040
 const path = require('path')
 const pUtil = require('picos-util')
 
